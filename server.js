728c2a53ef21eb773ed8ff11777fe33ede76f86c
Cain <Cain@CasketOnTheRun>
2014-02-15 21:16:29 +0000

    SocketServer test

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-var sys		= require('sys');
-var count 	= 0;
+var net     = require('net');
+var server  = net.createServer(function(connection)                             {
+    console.log('server connected');
+    connection.on('end', function() {
+        console.log('server disconnected');
+    });
+    connection.write('hello\r\n');
+    connection.pipe(connection);
+});
 
-sys.debug("Starting ...");
-
-function tick() 																	{
-	count	= count+1;
-	sys.debug("Tick count: " + count);
-	if (++count < 10) setTimeout(tick, 1000);
-	else sys.debug("...stoped!");
-}
-
-tick();
+server.listen(4000, function()                                                  {
+  console.log('server bound');
+});
