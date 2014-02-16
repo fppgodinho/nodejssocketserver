e0b1b47061fcdb367c548176547b2d56296de8f2
Fernando Godinho <fppgodinho@gmail.com>
2014-02-16 12:16:17 +0000

    update

--- ws.js
+++ ws.js
@@ -1,6 +1,6 @@
 var WebSocketServer = require('ws').Server;
 console.log('Start');
-var wss             = new WebSocketServer({port: 8080});
+var wss             = new WebSocketServer({port: 4000});
 wss.on('connection', function(ws)                                               {
     ws.on('message', function(message)                                          {
         console.log('received: %s', message);
